650728fddcedfb479fc70f0ecb42b19c9a380c0d
petar <petar@petar-zuvela.com>
2023-07-20 22:18:36 +0800

    -> Notebook Updates

--- src/analysis/_model_analysis.py
+++ src/analysis/_model_analysis.py
@@ -33,7 +33,8 @@ def analyze_model(
     y_all: ndarray,
     cv: Any,
     column_names: ndarray,
-    b_plot_feature_importances=True,
+    b_plot_y_randomization: bool = False,
+    b_plot_feature_importances: bool = True,
     title: str = 'QSRR Model'
 ) -> Tuple[DataFrame, DataFrame]:
 
@@ -47,8 +48,8 @@ def analyze_model(
     _y_bt_hat: ndarray = model.predict(x_bt).ravel()
 
     _predictions_df: DataFrame = pd.DataFrame()
-    _predictions_df["y"] = np.hstack((y_train.ravel(), y_validation.ravel(), y_bt.ravel()))
-    _predictions_df["y_hat"] = np.hstack((_y_train_hat.ravel(), _y_validation_hat.ravel(), _y_bt_hat.ravel()))
+    _predictions_df["y"] = np.hstack((y_train, y_validation, y_bt)).ravel()
+    _predictions_df["y_hat"] = np.hstack((_y_train_hat, _y_validation_hat, _y_bt_hat)).ravel()
     _predictions_df["residuals"] = _predictions_df["y_hat"] - _predictions_df["y"]
     _predictions_df["train_test"] = \
         ["Train" for _ in range(len(_y_train_hat))] \
@@ -59,70 +60,71 @@ def analyze_model(
     _hat_star, _hat_train, _hat_validation, _hat_bt, _res_scaled_train, _res_scaled_validation, _res_scaled_bt = \
         ApplicabilityDomain.calculate(
             x_train=x_train,
-            y_train=y_train.ravel(),
-            y_train_hat=_y_train_hat.ravel(),
+            y_train=y_train,
+            y_train_hat=_y_train_hat,
             x_validation=x_validation,
-            y_validation=y_validation.ravel(),
-            y_validation_hat=_y_validation_hat.ravel(),
+            y_validation=y_validation,
+            y_validation_hat=_y_validation_hat,
             x_bt=x_bt,
-            y_bt=y_bt.ravel(),
-            y_bt_hat=_y_bt_hat.ravel()
+            y_bt=y_bt,
+            y_bt_hat=_y_bt_hat
         )
 
-    _predictions_df["leverage"] = np.hstack((_hat_train.ravel(), _hat_validation.ravel(), _hat_bt.ravel()))
+    _predictions_df["leverage"] = np.hstack((_hat_train, _hat_validation, _hat_bt)).ravel()
     _predictions_df["scaled_residuals"] = np.hstack(
-        (_res_scaled_train.ravel(), _res_scaled_validation.ravel(), _res_scaled_bt.ravel())
-    )
+        (_res_scaled_train, _res_scaled_validation, _res_scaled_bt)
+    ).ravel()
 
     # Metrics
     _metrics_dict: Dict[str, Any] = {
-        "r2_train": Metrics.get_metric(y_train.ravel(), _y_train_hat.ravel(), MetricType.R2),
-        "rmse_train": Metrics.get_metric(y_train.ravel(), _y_train_hat.ravel(), MetricType.RMSE),
-        "r2_validation": Metrics.get_metric(y_validation.ravel(), _y_validation_hat.ravel(), MetricType.R2),
-        "rmse_validation": Metrics.get_metric(y_validation.ravel(), _y_validation_hat.ravel(), MetricType.RMSE),
-        "r2_bt": Metrics.get_metric(y_bt.ravel(), _y_bt_hat.ravel(), MetricType.R2),
-        "rmse_bt": Metrics.get_metric(y_bt.ravel(), _y_bt_hat.ravel(), MetricType.RMSE),
+        "r2_train": Metrics.get_metric(y_train, _y_train_hat, MetricType.R2),
+        "rmse_train": Metrics.get_metric(y_train, _y_train_hat, MetricType.RMSE),
+        "r2_validation": Metrics.get_metric(y_validation, _y_validation_hat, MetricType.R2),
+        "rmse_validation": Metrics.get_metric(y_validation, _y_validation_hat, MetricType.RMSE),
+        "r2_bt": Metrics.get_metric(y_bt, _y_bt_hat, MetricType.R2),
+        "rmse_bt": Metrics.get_metric(y_bt, _y_bt_hat, MetricType.RMSE),
     }
     _metrics_df: DataFrame = pd.DataFrame.from_dict(_metrics_dict, orient="index")
 
     # Predictive Ability Plot
     Visualizer.predictive_ability_plot(
-        y_train=y_train.ravel(),
-        y_train_hat=_y_train_hat.ravel(),
-        y_validation=y_validation.ravel(),
-        y_validation_hat=_y_validation_hat.ravel(),
-        y_bt=y_bt.ravel(),
-        y_bt_hat=_y_bt_hat.ravel()
+        y_train=y_train,
+        y_train_hat=_y_train_hat,
+        y_validation=y_validation,
+        y_validation_hat=_y_validation_hat,
+        y_bt=y_bt,
+        y_bt_hat=_y_bt_hat
     )
 
     # Residual Plot
     Visualizer.residual_plot(
-        y_train=y_train.ravel(),
-        y_train_hat=_y_train_hat.ravel(),
-        y_validation=y_validation.ravel(),
-        y_validation_hat=_y_validation_hat.ravel(),
-        y_bt=y_bt.ravel(),
-        y_bt_hat=_y_bt_hat.ravel()
+        y_train=y_train,
+        y_train_hat=_y_train_hat,
+        y_validation=y_validation,
+        y_validation_hat=_y_validation_hat,
+        y_bt=y_bt,
+        y_bt_hat=_y_bt_hat
     )
 
     # Applicability Domain Plot
     Visualizer.applicability_domain_plot(
-        hat_train=_hat_train.ravel(),
-        hat_validation=_hat_validation.ravel(),
-        hat_bt=_hat_bt.ravel(),
-        res_scaled_train=_res_scaled_train.ravel(),
-        res_scaled_validation=_res_scaled_validation.ravel(),
-        res_scaled_bt=_res_scaled_bt.ravel(),
+        hat_train=_hat_train,
+        hat_validation=_hat_validation,
+        hat_bt=_hat_bt,
+        res_scaled_train=_res_scaled_train,
+        res_scaled_validation=_res_scaled_validation,
+        res_scaled_bt=_res_scaled_bt,
         hat_star=_hat_star
     )
 
     # Y-Randomization Plot
-    Visualizer.y_randomization_plot(
-        model=model,
-        cv=cv,
-        x_train_all=x_train_all,
-        y_train_all=y_train_all.ravel()
-    )
+    if b_plot_y_randomization:
+        Visualizer.y_randomization_plot(
+            model=model,
+            cv=cv,
+            x_train_all=x_train_all,
+            y_train_all=y_train_all
+        )
 
     # Feature Importance Plots
     if b_plot_feature_importances:
@@ -130,7 +132,7 @@ def analyze_model(
         Visualizer.feature_importance_plot(
             model=model,
             x=x_all,
-            y=y_all.ravel(),
+            y=y_all,
             feature_importance_type=FeatureImportanceType.MeanImpurityDecrease,
             column_names=column_names
         )
@@ -138,7 +140,7 @@ def analyze_model(
         Visualizer.feature_importance_plot(
             model=model,
             x=x_all,
-            y=y_all.ravel(),
+            y=y_all,
             feature_importance_type=FeatureImportanceType.FeaturePermutation,
             column_names=column_names
         )
@@ -146,7 +148,7 @@ def analyze_model(
         Visualizer.feature_importance_plot(
             model=model,
             x=x_train_all,
-            y=y_train_all.ravel(),
+            y=y_train_all,
             feature_importance_type=FeatureImportanceType.SHAP,
             column_names=column_names
         )
