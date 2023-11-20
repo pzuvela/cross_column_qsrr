b2bded12ec370da0ef1b3d000190fccbb48aa3b3
petar <petar@petar-zuvela.com>
2023-11-20 12:45:40 +0800

    Fixed tests

--- qsrr/tests/tests_common/constants/_dataset_paths.py
+++ qsrr/tests/tests_common/constants/_dataset_paths.py
@@ -4,6 +4,18 @@ from qsrr.tests.tests_common.constants import Dataset
 
 class DatasetPaths:
 
-    DATA_PATH: str = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), Dataset.DATA)
+    DATA_PATH: str = os.path.join(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.dirname(
+                    os.path.dirname(
+                        os.path.dirname(os.path.abspath(__file__))
+                    )
+                )
+            )
+        ),
+        Dataset.DATA
+    )
+    print(DATA_PATH)
     SMRT_DATASET: str = os.path.join(DATA_PATH, "2023-11-18-smrt_dataset.csv")
     SMRT_DATASET_SMILES: str = os.path.join(DATA_PATH, "2023-11-18-smrt_dataset_smiles.csv")
